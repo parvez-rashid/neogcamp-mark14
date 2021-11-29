f6d2a63abc75a578f08df0289d509ab4548d7317
parvez-rashid <84724590+parvez-rashid@users.noreply.github.com>
2021-11-29 23:05:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ var resultBox = document.querySelector("#result");
 
 function calculateProfitAndLoss(initial, quantity, current) {
   if (current > initial) {
-    var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
+    var profit = ((current - initial) * quantity).toFixed(2);
+    var profitPercentage = ((profit / initial) * 100).toFixed(2);
     showOutput(
       `Profit is ${profit}. Profit percentage is ${profitPercentage}%`
     );
   } else if (current < initial) {
-    var loss = (initial - current) * quantity;
-    var lossPercentage = (loss / initial) * 100;
+    var loss = ((initial - current) * quantity).toFixed(2);
+    var lossPercentage = ((loss / initial) * 100).toFixed(2);
     showOutput(`Loss is ${loss}. Loss percentage is ${lossPercentage}%`);
   } else {
     showOutput("You have neither profit nor loss");
@@ -23,11 +23,22 @@ function calculateProfitAndLoss(initial, quantity, current) {
 }
 
 function submitHandler() {
-  var ip = Math.abs(initialPrice.value);
-  var qty = Math.abs(stockQuantity.value);
-  var curr = Math.abs(currentPrice.value);
-
-  calculateProfitAndLoss(ip, qty, curr);
+  if (
+    initialPrice.value === "" ||
+    stockQuantity.value === "" ||
+    currentPrice.value === ""
+  ) {
+    resultBox.innerText = "Please enter all values!";
+  } else {
+    var ip = Number(initialPrice.value);
+    var qty = Number(stockQuantity.value);
+    var curr = Number(currentPrice.value);
+    if (qty < 1) {
+      showOutput("Quantity should be at least be 1");
+    } else {
+      calculateProfitAndLoss(ip, qty, curr);
+    }
+  }
 }
 
 function showOutput(message) {
